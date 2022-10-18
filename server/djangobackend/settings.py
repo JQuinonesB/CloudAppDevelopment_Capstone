234ff09e631e6bf78484f6c7c889fe9d89da8044
Jairo <jairoa.quinonesb@gmail.com>
2022-10-18 12:00:30 -0500

    host

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'JairoBestCars.us-south.cf.appdomain.cloud/static']
+ALLOWED_HOSTS = ['JairoBestCars.us-south.cf.appdomain.cloud/static']
 
 
 # Application definition
